14d6170ab55bd0ec06e1248223ac37f5df83f86a
Joe <hutcheson2001@yahoo.com>
2022-04-10 11:46:56 -0400

    commit tree 810c4577901e8633e8c33922a0f44ce3c4c2ee8a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves arr
+  arr.map.with_index {|dwarf, i| puts "#{i + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet arr
+  arr.map{|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  long_word = arr.find{|call| call.length > 4}
+  if long_word == nil
+    false
+  else
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese arr
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find{|item| cheese_types.include?(item)}
 end
